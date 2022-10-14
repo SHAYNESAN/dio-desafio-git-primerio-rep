e1ed9f6963272813a8a9652f8ac35156f41b5567
SHAYNESAN <sandro.dutra1@hotmail.com>
2022-10-14 12:25:24 -0300

    Pandas

---
+++ Python/primeiroprograma.py
@@ -0,0 +1,22 @@
+"""print("Digite os valores da distancia, diametro1 e diametro2")
+entrada = input().split()
+distancia = int(entrada[0])
+diametro1 = int(entrada[1])
+diametro2 = int(entrada[2])
+# TODO: Calcule o ICM da comunicação dos Palatír de Sauron e Saruman, com 2 casas decimais no espaço #em branco abaixo:
+icm = distancia / (diametro1 + diametro2)
+print(f"O valor do ICM e {icm:.2f}")"""
+
+"""valores = input().split() 
+participantes = int(valores[0])
+cachorro_quente = int(valores[1])
+media = participantes / cachorro_quente
+print (f"{media:.2f}")"""
+
+"""valores = input().split()
+consumo = 12
+tempo = int(valores[0])
+velocidade = int(valores[1])
+gasto_gasolina = (velocidade * tempo) / consumo
+print(f"{gasto_gasolina:.3f}")"""
+
